47d0f238d16a4bdfd0fcaab7971d9f73cd1a9be0
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2025-02-16 00:07:15 -0800

    fix login page

--- hackathon/src/app/login/page.js
+++ hackathon/src/app/login/page.js
@@ -3,128 +3,102 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Login() {
-  // State for user input
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    // State for user input
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const router = useRouter();
+    // Function to handle login
+    const login = async (email, password) => {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/login/", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password }), // API expects "username"
+            });
+            const data = await response.json();
+            if (response.ok) {
+                localStorage.setItem("token", data.access_token);
+                console.log("Login successful!");
+            } else {
+                console.error("Login failed:", data.detail);
+            }
+        } catch (error) {
+            console.error("Error logging in:", error);
+        }
+    };
 
-  // Function to handle login
-  const login = async (email, password) => {
-    try {
-      const response = await fetch("http://127.0.0.1:8000/login/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }), // API expects "username"
-      });
-      const data = await response.json();
-      if (response.ok) {
-        //localStorage.setItem("token", data.access_token);
-        console.log("Login successful!");
-        router.push("/home");
-      } else {
-        console.error("Login failed:", data.detail);
-      }
-    } catch (error) {
-      console.error("Error logging in:", error);
-    }
-  };
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Prevents page reload
+        login(email, password);
+    };
 
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevents page reload
-    login(email, password);
-  };
-
-  return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-10 w-auto" src="cougar-head.jpg" alt="Coug Creative" />
-        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-          Sign In
-        </h2>
-      </div>
-
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                type="email"
-                id="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img className="mx-auto h-10 w-auto" src="cougar-head.jpg" alt="Coug Creative" />
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                    Sign In
+                </h2>
             </div>
-          </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                Password
-              </label>
-              <div className="text-sm">
-                <a href="#" className="font-semibold text-black hover:bg-gray-500 rounded-md p-1">
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                type="password"
-                id="password"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-                <div>
-                    <div className="flex items-center justify-between">
-                        <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                            Password
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                            Email address
                         </label>
-                        
-                    </div>
-                    <div className="mt-2">
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            required
-                            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                        />
-                        <div className="text-sm/6">
-                            <a href="#" className="font-semibold text-black hover:underline rounded-md p-1">
-                                Forgot password?
-                            </a>
+                        <div className="mt-2">
+                            <input
+                                type="email"
+                                id="email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
                         </div>
                     </div>
-                </div>
 
-                <div>
-                    <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-500">
-                        Sign in
-                    </button>
-                </div>
-            </form>
-            <p className="mt-10 text-center text-sm/6 text-gray-500">
-                Not a member?
-                <Link href="/register" className="font-semibold text-black hover:underline rounded-md p-1"> Register</Link>
-            </p>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-black hover:bg-gray-500 rounded-md p-1">
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                type="password"
+                                id="password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-500">
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+                    Not a member?
+                    <Link href="/register" className="font-semibold text-black hover:underline rounded-md p-1"> Register</Link>
+                </p>
+            </div>
         </div>
-      </div>
     );
 }
 
